43bb85f95f246d6e49f019cdcd45786a5600a646
Janel <janel.me@yahoo.com>
2020-08-10 03:37:11 +0430

    menu function unused elements removed

--- functions.py
+++ functions.py
@@ -1,8 +1,4 @@
 def menu(ui):
-    number = [0, 1, 2, 3, 4, 5, 6, 7]
-    item = ['Add', 'Remove', 'TestCharge', 'Run',
-            'Charges', 'Reset', 'Help', 'Exit'
-            ]
     ui.colored_print('1.Add    2.Remove    3.TestCharge ', 'printb')
     ui.colored_print('4.Run    5.Charges   6.Reset      ', 'printb')
     ui.colored_print('7.Help   8.Exit                   ', 'printb')
